65d7d2ed6b4d96028813c608daf6d0fb582ca872
mateusntn <mateusantoniodev@gmail.com>
2021-10-06 00:21:22 -0300

    Rename activity1 to stadium

--- module3/stadium.java
+++ module3/stadium.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class activity1 {
+public class stadium {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int categoryA = 50;
