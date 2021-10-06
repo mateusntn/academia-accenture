d045f0f38615de3c402e5a1e07f6023ed724accb
mateusntn <mateusantoniodev@gmail.com>
2021-10-05 21:53:47 -0300

    Create Activity1

---
+++ module3/activity1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class activity1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int categoryA = 50;
+        int categoryB = 30;
+        int categoryC = 10;
+        
+        System.out.print("How many Class A tickets were sold? ");
+        int quantityA = scanner.nextInt();
+        int valueA = quantityA * categoryA;
+
+        System.out.print("How many Class B tickets were sold? ");
+        int quantityB = scanner.nextInt();
+        int valueB = quantityB * categoryB;
+
+        System.out.print("How many Class C tickets were sold? ");
+        int quantityC = scanner.nextInt();
+        int valueC = quantityC * categoryC;
+
+        int totalQuantity = quantityA + quantityB + quantityC;
+        int totalValue = valueA + valueB + valueC;
+
+        System.out.println("Class A: " + quantityA + " tickets - R$" + valueA);
+        System.out.println("Class B: " + quantityB + " tickets - R$" + valueB);
+        System.out.println("Class C: " + quantityC + " tickets - R$" + valueC);
+
+
+        System.out.println("Total: "+ totalQuantity +" tickets - R$"+ totalValue);
+    }
+}
