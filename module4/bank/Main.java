34e69273cccbf3af8819941a6202c811a1fa125f
mateusntn <mateusantoniodev@gmail.com>
2021-10-08 12:00:46 -0300

    Bank Activity: Modify the main program

--- module4/bank/Main.java
+++ module4/bank/Main.java
@@ -1,18 +1,15 @@
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         Client client1 = new Client("Mateus", "182.112.462-80", "Antonio");
-        Account account1 = new Account(1, client1, 784,LocalDate.parse("06-10-2021", dtf));
+        Account account1 = new Account(1, client1, 784, LocalDate.now());
         account1.Client.DisplayName();
         account1.DisplayBalance();
-        
         account1.Deposit(16);
 
         Client client2 = new Client("Julio", "167.342.563-80", "Vieira");
-        Account account2 = new Account(2, client2, 1200,LocalDate.parse("10-10-2021", dtf));
+        Account account2 = new Account(2, client2, 1200, LocalDate.now());
         account2.Client.DisplayName();
         account2.DisplayBalance();
 
